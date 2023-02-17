78f553ccb57d98d4ee613785d782933f059a682d
CristnaAT <alumno.668286@ies-azarquiel.es>
2023-02-17 09:06:37 +0100

    Hasta minuto 03:25:34 (se añade login de usuario,password se almacena sin función hash,no hay todavía gestión de inicio de sesión, modificaciones respecto del curso: - fetch cambio const request a response - respuesta de /api/login de text a JSON)

--- src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
+++ src/main/java/com/s2daw/demo/dao/UsuarioDaoImp.java
@@ -15,6 +15,7 @@ public class UsuarioDaoImp implements UsuarioDao {
     EntityManager entityManager;
 
     @Override
+    @Transactional
     public List<Usuario> getUsuarios() {
         String query = "FROM Usuario";
         return entityManager.createQuery(query).getResultList();
@@ -31,4 +32,14 @@ public class UsuarioDaoImp implements UsuarioDao {
         entityManager.merge(usuario);
     }
 
+    @Override
+    public boolean verificarCredenciales(Usuario usuario){
+        String query = "FROM Usuario WHERE email = :email AND password = :password";
+        List<Usuario> lista = entityManager.createQuery(query)
+                .setParameter("email", usuario.getEmail())
+                .setParameter("password", usuario.getPassword())
+                .getResultList();
+
+        return !lista.isEmpty();
+    }
 }
